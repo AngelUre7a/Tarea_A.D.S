fbfd13e3dca0c5f440a375ce2a75acd8aa56f50c
AngelUreña <129628027+AngelUre7a@users.noreply.github.com>
2025-03-15 19:42:40 -0600

    Initial commit

---
+++ src/main/java/cr/ac/una/tarea_a/d/s/PrimaryController.java
@@ -0,0 +1,12 @@
+package cr.ac.una.tarea_a.d.s;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cr.ac.una.tarea_a.d.s {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+
+    opens cr.ac.una.tarea_a.d.s to javafx.fxml;
+    exports cr.ac.una.tarea_a.d.s;
+}
