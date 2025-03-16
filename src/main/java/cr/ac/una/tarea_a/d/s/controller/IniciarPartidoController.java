e72aea3e7a60e0253d735bf047a78416c159cc02
DasVargas <daslynvargasgambo@gmail.com>
2025-03-15 20:15:15 -0600

    prueba 1
    system println en iniciar partida

--- src/main/java/cr/ac/una/tarea_a/d/s/controller/IniciarPartidoController.java
+++ src/main/java/cr/ac/una/tarea_a/d/s/controller/IniciarPartidoController.java
@@ -20,6 +20,7 @@ public class IniciarPartidoController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("prueba");
         // TODO
     }    
     
