8ba61e164572af228b69f97d7e2b19984c1ddfb3
DasVargas <daslynvargasgambo@gmail.com>
2025-03-15 20:07:07 -0600

    Initial commit das
    ..

--- src/main/java/cr/ac/una/tarea_a/d/s/App.java
+++ src/main/java/cr/ac/una/tarea_a/d/s/App.java
@@ -33,6 +33,7 @@ public class App extends Application {
 
     public static void main(String[] args) {
         System.out.println("Hola");
+            System.out.println("Hola 2.0");
         launch();
     }
 
