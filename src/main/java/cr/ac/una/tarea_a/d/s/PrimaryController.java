2f2433c2d4e956d6a7c0238081ac74d1518b3d13
AngelUreña <129628027+AngelUre7a@users.noreply.github.com>
2025-03-16 00:59:06 -0600

    1

--- src/main/java/cr/ac/una/tarea_a/d/s/PrimaryController.java
+++ src/main/java/cr/ac/una/tarea_a/d/s/PrimaryController.java
@@ -2,8 +2,12 @@ package cr.ac.una.tarea_a.d.s;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+
+    @FXML
+    private Button primaryButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
