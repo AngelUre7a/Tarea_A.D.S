a0f1e66f78083a3e706755dc7a954de1a5b359f4
AngelUreña <129628027+AngelUre7a@users.noreply.github.com>
2025-03-16 23:22:39 -0600

    refact(menucontroller)=btnregistrodeporte

--- src/main/java/cr/ac/una/tarea_a/d/s/controller/MenuController.java
+++ src/main/java/cr/ac/una/tarea_a/d/s/controller/MenuController.java
@@ -42,7 +42,7 @@ public class MenuController implements Initializable {
 
     @FXML
     private void onActionBtnRegistroDeportes(ActionEvent event) throws IOException {
-          App.setRoot("RegistroDeporte");
+          App.setRoot("RegistroListaDeporteBalon");
     }
 
     @FXML
