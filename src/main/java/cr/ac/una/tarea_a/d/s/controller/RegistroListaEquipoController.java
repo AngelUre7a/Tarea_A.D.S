1c6226ed10c4d2f82ad73044e093c14d5e8b408a
DasVargas <daslynvargasgambo@gmail.com>
2025-03-17 20:49:54 -0600

    refactor(lista E y D): cambio en las vistas
    cambie los botones y posicion en la pantalla de lista de deportes y lista de equipos

--- src/main/java/cr/ac/una/tarea_a/d/s/controller/RegistroListaEquipoController.java
+++ src/main/java/cr/ac/una/tarea_a/d/s/controller/RegistroListaEquipoController.java
@@ -4,8 +4,11 @@
  */
 package cr.ac.una.tarea_a.d.s.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -15,6 +18,11 @@ import javafx.fxml.Initializable;
  */
 public class RegistroListaEquipoController implements Initializable {
 
+    @FXML
+    private MFXButton btnAgregar;
+    @FXML
+    private MFXButton btnVolver;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +30,13 @@ public class RegistroListaEquipoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onActionBtnAgregar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnVolver(ActionEvent event) {
+    }
     
 }
