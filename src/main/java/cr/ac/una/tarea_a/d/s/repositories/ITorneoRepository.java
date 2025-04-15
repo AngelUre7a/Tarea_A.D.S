d419b39d1645ccf32c6a4da7145b2a579db5aafc
DasVargas <daslynvargasgambo@gmail.com>
2025-04-15 00:38:58 -0600

    Revert "Feat(repositories): se agrego repositorios para partida"
    This reverts commit 1e5c8601452b333c3c5d88983e0b589bc144f697.

--- src/main/java/cr/ac/una/tarea_a/d/s/repositories/ITorneoRepository.java
+++ src/main/java/cr/ac/una/tarea_a/d/s/repositories/ITorneoRepository.java
@@ -4,18 +4,16 @@ import cr.ac.una.tarea_a.d.s.model.Torneo;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-
 /**
  *
  * @author dasly
  */
 public interface ITorneoRepository {
-
     Torneo save(Torneo torneo) throws IOException;
 
-    List<Torneo> findAll() throws IOException;
+List<Torneo> findAll() throws IOException;
 
-    Optional<Torneo> findById(String id) throws IOException;
+Optional<Torneo> findById(String id) throws IOException;
 
-    boolean deleteById(String id) throws IOException;
+boolean deleteById(String id) throws IOException;
 }
