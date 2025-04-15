1e5c8601452b333c3c5d88983e0b589bc144f697
DasVargas <daslynvargasgambo@gmail.com>
2025-04-15 00:36:13 -0600

    Feat(repositories): se agrego repositorios para partida

--- src/main/java/cr/ac/una/tarea_a/d/s/repositories/IPartidaRepository.java
+++ src/main/java/cr/ac/una/tarea_a/d/s/repositories/IPartidaRepository.java
@@ -1,9 +1,20 @@
 package cr.ac.una.tarea_a.d.s.repositories;
-
+import cr.ac.una.tarea_a.d.s.model.Partida;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 /**
  *
  * @author dasly
  */
 public interface IPartidaRepository {
-    
+
+    Partida save(Partida partida) throws IOException;
+
+    List<Partida> findAll() throws IOException;
+
+    Optional<Partida> findById(String id) throws IOException;
+
+    boolean deleteById(String id) throws IOException;
+
 }
